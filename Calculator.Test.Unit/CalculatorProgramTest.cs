f9043af3838110f8c035d0390c76273438f2b7e6
kbjerringfelt <karen.bjerringfelt@gmail.com>
2019-01-30 11:39:51 +0100

    Add project files.

---
+++ Calculator.Test.Unit/CalculatorProgramTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CalculatorProgram.Test.Unit
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+       public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+    }
+}
---
+++ CalculatorUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("86a6f2e4-d2b0-499b-83ec-46aadfc216c8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
