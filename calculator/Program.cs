74c95d66c3d501fcb390ba11a7ba589d47609b3e
Kajene <au594051@post.au.dk>
2019-02-04 18:17:15 +0100

    Tilføjede program

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -11,7 +11,24 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-          
+            // Declare the unit-under-test
+            var uut = new Calculator();
+
+            // Test Add()
+            Console.WriteLine("Add({0}, {1}) = {2}", 3.5, 2.5, uut.Add(3.5, 2.5));
+            Console.WriteLine("Add({0}, {1}) = {2}", -3.5, 2.5, uut.Add(-3.5, 2.5));
+            Console.WriteLine("Add({0}, {1}) = {2}", -3.5, -2.5, uut.Add(-3.5, -2.5));
+
+            // Test Subtract()
+            Console.WriteLine("Subtract({0}, {1}) = {2}", 3.5, 2.5, uut.Subtract(3.5, 2.5));
+            Console.WriteLine("Subtract({0}, {1}) = {2}", -3.5, 2.5, uut.Subtract(-3.5, 2.5));
+            Console.WriteLine("Subtract({0}, {1}) = {2}", -3.5, -2.5, uut.Subtract(-3.5, -2.5));
+
+            // Test Multiply()
+            Console.WriteLine("Multiply({0}, {1}) = {2}", 3.5, 2.5, uut.Multiply(3.5, 2.5));
+            Console.WriteLine("Multiply({0}, {1}) = {2}", -3.5, 2.5, uut.Multiply(-3.5, 2.5));
+            Console.WriteLine("Multiply({0}, {1}) = {2}", -3.5, -2.5, uut.Multiply(-3.5, -2.5));
+
 
         }
     }
