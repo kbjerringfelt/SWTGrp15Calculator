c94bdc4f14ef978df1e588de2da981abc5d90977
Kajene <au594051@post.au.dk>
2019-02-04 18:01:27 +0100

    Update Program.cs

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -14,10 +14,7 @@ namespace calculator
             // Declare the unit-under-test
             var uut = new Calculator();
 
-            // Test Add()
-            Console.WriteLine("Add({0}, {1}) = {2}", 3.5, 2.5, uut.Add(3.5, 2.5));
-            Console.WriteLine("Add({0}, {1}) = {2}", -3.5, 2.5, uut.Add(-3.5, 2.5));
-            Console.WriteLine("Add({0}, {1}) = {2}", -3.5, -2.5, uut.Add(-3.5, -2.5));
+            
 
             // Test Subtract()
             Console.WriteLine("Subtract({0}, {1}) = {2}", 3.5, 2.5, uut.Subtract(3.5, 2.5));
