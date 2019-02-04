569f5fe04631a4af8e7c9eaba4aff76fa0bf9f0a
Kajene <au594051@post.au.dk>
2019-02-04 18:11:32 +0100

    TEst

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -14,7 +14,10 @@ namespace calculator
             // Declare the unit-under-test
             var uut = new Calculator();
 
-            
+            // Test Add()
+            Console.WriteLine("Add({0}, {1}) = {2}", 3.5, 2.5, uut.Add(3.5, 2.5));
+            Console.WriteLine("Add({0}, {1}) = {2}", -3.5, 2.5, uut.Add(-3.5, 2.5));
+            Console.WriteLine("Add({0}, {1}) = {2}", -3.5, -2.5, uut.Add(-3.5, -2.5));
 
             // Test Subtract()
             Console.WriteLine("Subtract({0}, {1}) = {2}", 3.5, 2.5, uut.Subtract(3.5, 2.5));
@@ -26,10 +29,6 @@ namespace calculator
             Console.WriteLine("Multiply({0}, {1}) = {2}", -3.5, 2.5, uut.Multiply(-3.5, 2.5));
             Console.WriteLine("Multiply({0}, {1}) = {2}", -3.5, -2.5, uut.Multiply(-3.5, -2.5));
 
-            // Test Power()
-            Console.WriteLine("Power({0}, {1}) = {2}", 2.0, 3.0, uut.Power(2.0, 3.0));
-            Console.WriteLine("Power({0}, {1}) = {2}", -2.0, 3.0, uut.Power(-2.0, 3.0));
-            Console.WriteLine("Power({0}, {1}) = {2}", -2.0, -3.0, uut.Power(-2.0, -3.0));
         }
     }
 }
